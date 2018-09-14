9582a904dd96598a97c19248b79a6783ba1c5199
Michael Schumann <schumann82@gmail.com>
2018-09-13 21:37:38 -0700

    took out index model

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var request = require("request");
-const db = require("./models");
+
+var Article = require("./models/Article");
+var Note = require("./models/Note");
 
 const PORT = process.env.PORT || 8080;
 
@@ -97,7 +99,7 @@ app.get("/scrape", function(req, res) {
 
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
+  Article.find({})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
@@ -111,7 +113,7 @@ app.get("/articles", function(req, res) {
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
+  Article.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
     .populate("note")
     .then(function(dbArticle) {
@@ -127,12 +129,12 @@ app.get("/articles/:id", function(req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
+  Note.create(req.body)
     .then(function(dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
@@ -146,7 +148,7 @@ app.post("/articles/:id", function(req, res) {
 
 app.get("/notes", function(req, res) {
   // Grab every document in the Articles collection
-  db.Note.find({})
+  Note.find({})
     .then(function(dbNote) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbNote);
@@ -172,7 +174,7 @@ app.post("/articles", function(req, res) {
       
 			result.post = post;
 
-  db.Article.create(result)
+  Article.create(result)
         .then(function(dbArticle) {
           // View the added result in the console
           // console.log(dbArticle + "db article");
@@ -188,7 +190,7 @@ app.post("/articles", function(req, res) {
 
 app.get("/notes/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Note.findOne({ _id: req.params.id })
+  Note.findOne({ _id: req.params.id })
     
     .then(function(dbNote) {
       
@@ -202,7 +204,7 @@ app.get("/notes/:id", function(req, res) {
 
 app.delete("/articles/:id" , function(req, res) {
   
-  db.Article.remove({ _id: req.params.id })
+  Article.remove({ _id: req.params.id })
     .then(function(dbArticle) {
       
       res.json(dbArticle);
@@ -215,7 +217,7 @@ app.delete("/articles/:id" , function(req, res) {
 
 app.post("/notes/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Note.update({ _id: req.params.id }, {$set: {"title": req.body.title, "body": req.body.body}})
+  Note.update({ _id: req.params.id }, {$set: {"title": req.body.title, "body": req.body.body}})
     
     .then(function(dbNote) {
       
